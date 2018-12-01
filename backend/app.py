70a592b2481ae34b696e4730742904ae420623f4
AIM1993 <244018719@qq.com>
2018-12-01 14:48:09 +0800

    完成后端restfulAPI构建

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,51 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
+from flask_cors import CORS
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, TextAreaField, DateField, IntegerField, validators
 
 app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'xza3363199--AIMI'
+app.config['MYSQL_DB'] = 'flaskvuesite'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
+CORS(app)
 api = Api(app)
 
-class HelloWorld(Resource):
+todos = {}
+
+
+
+class ArticleForm(Form):
+    name = StringField('Name', [validators.Length(min=1, max=100)])
+    birth = DateField('Birth', [validators.Length(min=30)])
+    score = IntegerField('Score', [validators.Length(min=0)])
+
+
+
+class Helloworld(Resource):
     def get(self):
-        return {'hello': 'world'}
+        form = ArticleForm(request.form)
+        cur = mysql.connection.cursor()
+        result_data = cur.execute("SELECT * FROM flaskvuesite")
+        data = cur.fetchall()
+        return jsonify({
+            'result': "Score Table",
+            'data': data
+        })
+    
+    def post(self):
+        some_json = request.get_json()
+        return {'you sent': some_json}
 
-api.add_resource(HelloWorld, '/')
 
-if __name__ == 'main':
+
+
+api.add_resource(Helloworld, '/')
+
+if __name__ == '__main__':
     app.run(debug=True)
