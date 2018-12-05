5f1ba51e890e92d3d469c255a64d505f38960fca
AIM1993 <244018719@qq.com>
2018-12-05 15:50:56 +0800

    进行全面优化

--- frontend/src/components/Scoretable.vue
+++ frontend/src/components/Scoretable.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="container" class="hello">
-    <h2>{{city}}</h2>
+    <h2>{{tableTitle}}</h2>
     <label class="label">最近7天天气情况</label>
+    <!--Alert-->
+    <div class="callout alert" data-closable v-if="warningFlag">
+      <h5>未选择城市!</h5>
+      <p>请在下方点选需要查询的城市名称。</p>
+      <button @click="resetWarningFlag" class="close-button" aria-label="Dismiss alert" type="button">
+        <span>&times;</span>
+      </button>
+    </div>
+
     <hr class="hr"/>
     <div class="grid-x grid-padding-x">
     <fieldset class="large-5 cell">
       <legend>选择城市</legend>
-      <input type="radio" name="pokemon" v-model="input_city" value="成都" id="pokemonRed" required><label for="pokemonRed">成都</label>
-      <input type="radio" name="pokemon" v-model="input_city" value="上海" id="pokemonBlue"><label for="pokemonBlue">上海</label>
+      <input type="radio" name="pokemon" v-model="inputedCity" value="成都" id="pokemonRed" required><label for="pokemonRed">成都</label>
+      <input type="radio" name="pokemon" v-model="inputedCity" value="上海" id="pokemonBlue"><label for="pokemonBlue">上海</label>
+      <button id="submit" @click="loadData" class="primary button">获取天气</button>
     </fieldset>
     </div>
-    <button id="submit" @click="loadData" class="primary button">获取天气</button>
+    
     <table id="table" class="table unstriped">
       <thead>
         <tr>
@@ -31,7 +41,8 @@
         </tr>
       </tbody>
     </table>
-    <div id="chart">
+
+    <div id="chart" v-show="cityChoosed">
       <canvas id="myChart"></canvas>
     </div>
 
@@ -43,24 +54,27 @@
     data() {
       return {
         status: '',
-        city: '',
-        input_city: '',
+        tableTitle: '',
+        inputedCity: '',
         table_data: '',
+        cityChoosed: false,
+        warningFlag: false
       }
     },
 
     created: function () {
-      this.city = "城市天气数据查询";
+      this.tableTitle = "城市天气数据查询";
     },
 
     methods: {
       loadData: function () {
-        this.status = "数据获取成功!";
         console.log(this.status);
         var table = this;
-        this.$ajax.get('http://localhost:5000/v1/' + this.input_city)
+        this.$ajax.get('http://localhost:5000/v1/' + this.inputedCity)
           .then((response) => {
-            this.status = this.city;
+            this.status = "OK";
+            this.cityChoosed = true;
+            this.tableTitle = this.inputedCity;
             this.table_data = response.data.table.result.future;
             console.log(response.data.table.result);
           })
@@ -68,46 +82,52 @@
             this.status = "Error occured." + error;
             console.log(this.status);
           });
+          if (this.inputedCity === '') {
+            this.warning();
+          } else {
+          this.loadChart();
+          }
+        },
 
-        if ((this.input_city === '')) {
-          alert("请输入正确内容！");
-        } else {
-            this.loadChart();
-        }
-
+      warning: function () {
+        this.warningFlag = true;
       },
 
-    loadChart: function () {
-      var ctx = document.getElementById('myChart').getContext('2d');
-      var chart = new Chart(ctx, {
-          // The type of chart we want to create
-          type: 'line',
+      resetWarningFlag: function () {
+        this.warningFlag = false;
+      },
 
-          // The data for our dataset
-          data: {
-              title: "温度走势图",
-              labels: ["星期二", "星期三", "星期四", "星期五", "星期六", "星期天", "星期一"],
-              datasets: [
-                {
-                  label: "最低气温",
-                  backgroundColor: 'rgba(	0, 191, 255, 0.75)',
-                  borderColor: 'rgb(0, 191, 255)',
-                  data: [8, 7, 4, 3, 3, 7, 3],
-                },
-                {
-                  label: "最高气温",
-                  backgroundColor: 'rgba(220, 20, 60, 0.75)',
-                  borderColor: 'rgba(220, 20, 60, 0.75)',
-                  data: [12, 11, 10, 8, 6, 11, 8],
-                },
-              ]
-          },
+      loadChart: function () {
+        var ctx = document.getElementById('myChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
 
-          // Configuration options go here
-          options: {}
-        });
+            // The data for our dataset
+            data: {
+                title: "温度走势图",
+                labels: ["星期二", "星期三", "星期四", "星期五", "星期六", "星期天", "星期一"],
+                datasets: [
+                  {
+                    label: "最低气温",
+                    backgroundColor: 'rgba(	0, 191, 255, 0.75)',
+                    borderColor: 'rgb(0, 191, 255)',
+                    data: [8, 7, 4, 3, 3, 7, 3],
+                  },
+                  {
+                    label: "最高气温",
+                    backgroundColor: 'rgba(220, 20, 60, 0.75)',
+                    borderColor: 'rgba(220, 20, 60, 0.75)',
+                    data: [12, 11, 10, 8, 6, 11, 8],
+                  },
+                ]
+            },
+
+            // Configuration options go here
+            options: {}
+          });
+        }
       }
-    }
   }
 
 </script>
@@ -120,7 +140,7 @@
     padding: 1.2%;
     margin: 20px 15%;
     background: #1111;
-    min-height: 84vh;
+    min-height: 86vh;
     font-family: sans-serif;
   }
 
